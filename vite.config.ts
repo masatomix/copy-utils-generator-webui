65f3caed1151dcf775a71af6379d657eeb8cb651
Masatomi KINO <masatomix@ki-no.org>
2025-06-11 21:35:00 +0900

    デプロイの設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+// import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
+  base: "/copy-utils-generator-webui/",
   plugins: [
     react(),
+    // nodePolyfills()
   ],
   define: {
     "process.env": {},
