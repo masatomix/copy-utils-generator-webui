e7095ed16f83d876bfff7fd4288293f549d9476c
Masatomi KINO <masatomix@ki-no.org>
2025-07-15 19:13:25 +0900

    タスク一覧のフッタを共通化したタグを追加

---
+++ src/components/TaskRowsFooter.tsx
@@ -0,0 +1,64 @@
+import { TableFooter, TableRow, TableCell } from "@mui/material";
+import type { TaskRow } from "evmtools-node/domain";
+import { useMemo } from "react";
+import { formatNumberIntl } from "../utils/format";
+
+export const TaskRowsFooter = ({
+  taskRows,
+  baseDate,
+}: {
+  taskRows: TaskRow[];
+  baseDate: Date;
+}) => {
+  const totals = useMemo(() => {
+    const workload = taskRows.reduce((sum, t) => sum + (t.workload ?? 0), 0);
+    const pv = taskRows.reduce((sum, t) => sum + (t.pv ?? 0), 0);
+    const ev = taskRows.reduce((sum, t) => sum + (t.ev ?? 0), 0);
+    const spi = pv !== 0 ? ev / pv : NaN;
+    const progressRate = workload !== 0 ? ev / workload : NaN;
+    return {
+      workload,
+      pvToday: taskRows.reduce(
+        (sum, t) => sum + (t.calculatePV(new Date(baseDate)) ?? 0),
+        0
+      ),
+      progressRate,
+      pv,
+      ev,
+      spi,
+    };
+  }, [taskRows, baseDate]);
+  return (
+    <TableFooter>
+      <TableRow>
+        <TableCell colSpan={3} align="right">
+          合計
+        </TableCell>
+        <TableCell align="right">
+          {formatNumberIntl(totals.workload, {
+            maximumFractionDigits: 2,
+          })}
+        </TableCell>
+        <TableCell align="right"></TableCell>
+        <TableCell align="right">
+          {formatNumberIntl(totals.pvToday, {
+            maximumFractionDigits: 2,
+          })}
+        </TableCell>
+        <TableCell colSpan={2}></TableCell>
+        <TableCell>
+          {formatNumberIntl(totals.progressRate, {
+            style: "percent",
+            maximumFractionDigits: 1,
+          })}
+        </TableCell>
+        <TableCell></TableCell>
+        <TableCell align="right">
+          {formatNumberIntl(totals.pv, { maximumFractionDigits: 2 })} /{" "}
+          {formatNumberIntl(totals.ev, { maximumFractionDigits: 2 })} /{" "}
+          {formatNumberIntl(totals.spi, { maximumFractionDigits: 2 })}
+        </TableCell>
+      </TableRow>
+    </TableFooter>
+  );
+};
