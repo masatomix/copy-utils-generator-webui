6173f2570e81d8e0c0a6ee6c626403d3e2fc991e
Masatomi KINO <masatomix@ki-no.org>
2025-07-15 17:41:34 +0900

    微調整

--- src/components/ProjectDiffSummary.tsx
+++ src/components/ProjectDiffSummary.tsx
@@ -36,14 +36,14 @@ export const ProjectDiffSummary = ({
           alignItems="center"
         >
           <Stack>
-            <Typography gutterBottom>
+            <Typography variant="body2" mt={1}>
               進捗率、PV、EVに変更があったタスクのプロジェクト単位での集計値です。
             </Typography>
-            <Typography gutterBottom>
+            <Typography variant="body2" mt={1}>
               <strong>作業予定タスクのPV合計:</strong>
               プロジェクト全体で予定していたタスクたちの「基準日の」PV合計。カッコ内はそのタスクたちの「比較対象日までの累積PV→基準日までの累積PV」を表示
             </Typography>
-            <Typography gutterBottom>
+            <Typography variant="body2" mt={1}>
               <strong>進捗したタスクのEV合計:</strong>
               プロジェクト全体で基準日に進捗したタスク達のEV合計。カッコ内はそのタスクたちの「比較対象日までの累積EV→基準日までの累積EV」を表示
             </Typography>
